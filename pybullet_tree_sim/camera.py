00d324aacec0b754bc732eacc5e4a73972a229db
lukestroh <luke.strohbehn@gmail.com>
2024-10-22 13:30:24 -0700

    inheritance sensor->camera->ToF

--- pybullet_tree_sim/camera.py
+++ pybullet_tree_sim/camera.py
@@ -10,15 +10,15 @@ import os
 from zenlog import log
 
 class Camera(Sensor):
-    def __init__(self, name: str, sensor_type: str='camera') -> None:
+    def __init__(self, sensor_name: str, sensor_type: str='camera') -> None:
         """Builds a camera object from a base Sensor class"""
-
+        super().__init__(sensor_name=sensor_name, sensor_type=sensor_type)
         return
 
 
 
 def main():
-    camera = Camera(type="realsense_d435i")
+    camera = Camera(sensor_name="realsense_d435i")
     print(camera.params)
     return
 
