b2b760439b117ee8383a9197d4bd52583c136b5d
lukestroh <luke.strohbehn@gmail.com>
2024-10-22 13:30:24 -0700

    inheritance sensor->camera->ToF

--- pybullet_tree_sim/time_of_flight.py
+++ pybullet_tree_sim/time_of_flight.py
@@ -10,35 +10,20 @@ import os
 from zenlog import log
 
 class TimeOfFlight(Camera):
-    def __init__(self, type: str) -> None:
-
-        self.params = self._load_tof_params(type=type)
-        print(self.params)
+    def __init__(self, sensor_name: str, sensor_type: str = "tof") -> None:
+        super().__init__(sensor_name=sensor_name, sensor_type=sensor_type)
         return
-        
-        
-    def _load_tof_params(self, type: str) -> dict:
-        """
-        @param type: The type of ToF to be used.
-        """
-        tof_conf_path = os.path.join(TOFS_PATH, f"{type}.yaml")
-        if os.path.exists(tof_conf_path):
-            log.info(f"Loading camera configuration from {tof_conf_path}")
-            tof_conf_content = yutils.load_yaml(tof_conf_path)
-            if tof_conf_content is not None:
-                return tof_conf_content
-            else:
-                raise Exception(f"Failed to load camera configuration from {tof_conf_path}")
-        else:
-            raise FileNotFoundError(f"Camera configuration not found at {tof_conf_path}")
-        
-        
+
+
+
+
 def main():
-    
-    tof = TimeOfFlight(type="vl53l8cx")
-    
+
+    tof = TimeOfFlight(sensor_name="vl53l8cx")
+    print(tof.params)
+
     return
-    
-    
+
+
 if __name__ == "__main__":
     main()
