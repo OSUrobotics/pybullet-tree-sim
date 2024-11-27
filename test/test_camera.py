c239ed4015a928219019a611cd0ff50ea3dcee23
clairefollett <chlaw101@gmail.com>
2024-11-27 02:38:01 +0000

    added tests for the camera class. test_aspect_ratio, test_camera_initialization, test_custom_resolution, test_depth_film_coords_normalization. added test_yaml files

--- test/test_camera.py
+++ test/test_camera.py
@@ -11,6 +11,33 @@ class TestCamera(unittest.TestCase):
     pbutils = PyBUtils(renders=False)
     camera = Camera(pbutils.pbclient, sensor_name="realsense_d435i")
 
+    def test_aspect_ratio(self):
+        """Test that aspect ratio is calculated correctly."""
+        aspect_ratio = self.camera.depth_width / self.camera.depth_height
+        self.assertAlmostEqual(aspect_ratio, self.camera.depth_width / self.camera.depth_height)
+        return
+
+    def test_camera_initialization(self):
+        """Test that camera parameters are initialized correctly."""
+        self.assertEqual(self.camera.depth_width, self.camera.params["depth"]["width"])
+        self.assertEqual(self.camera.depth_height, self.camera.params["depth"]["height"])
+        self.assertAlmostEqual(self.camera.vfov, self.camera.params["depth"]["vfov"])
+        self.assertAlmostEqual(self.camera.hfov, self.camera.params["depth"]["hfov"])
+        self.assertAlmostEqual(self.camera.near_val, self.camera.params["depth"]["near_plane"])
+        self.assertAlmostEqual(self.camera.far_val, self.camera.params["depth"]["far_plane"])
+        return
+    
+    def test_custom_resolution(self):
+        """Test camera initialization with test_camera.yaml."""
+        custom_camera = Camera(self.pbutils.pbclient, sensor_name="test_camera")
+        custom_camera.depth_width = 640
+        custom_camera.depth_height = 480
+        custom_camera.depth_pixel_coords = np.array(
+            list(np.ndindex((custom_camera.depth_width, custom_camera.depth_height))), dtype=int
+        )
+        self.assertEqual(custom_camera.depth_pixel_coords.shape, (640 * 480, 2))
+        return
+
     def test_depth_pixel_coords_range(self):
         """Test that the pixel coordinates are in the range x=[0, depth_width] and y=[0, depth_height]."""
         self.assertTrue(self.camera.depth_pixel_coords[0, 0] == 0)
@@ -25,6 +52,13 @@ class TestCamera(unittest.TestCase):
         self.assertTrue(self.camera.depth_film_coords[0, 1] > -1)
         self.assertTrue(self.camera.depth_film_coords[-1, 0] < 1)
         self.assertTrue(self.camera.depth_film_coords[-1, 1] < 1)
+        return
+    
+    def test_depth_film_coords_normalization(self):
+        """Test that film coordinates are normalized to [-1, 1]."""
+        coords = self.camera.depth_film_coords
+        self.assertTrue(np.all(coords >= -1))
+        self.assertTrue(np.all(coords <= 1))
         return
 
     def test_xy_depth_projection(self):
